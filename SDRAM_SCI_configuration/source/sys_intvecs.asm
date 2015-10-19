c10f10108e7dc1d38223d521beba19e4950ba5ab
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2015-10-19 22:31:29 +0200

    Change for TMS570 exception vectors structure for overlaying applied to HDK kit as well.
    Code is tested with RTEMS TMS570 port that it works as expected.
    The RTEMS image start with exception jump trampolines has
    been remapped to addresses 0x0040 to 0x007f by use of POM.
    
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- SDRAM_SCI_configuration/source/sys_intvecs.asm
+++ SDRAM_SCI_configuration/source/sys_intvecs.asm
@@ -12,25 +12,74 @@
 
     .ref _c_int00
     .ref _dabort
-    
+
     .def resetEntry
 
 ;-------------------------------------------------------------------------------
 ; interrupt vectors
+;
+; the vectors are served by branch instructions which
+; transfer execution to addresses specified by corresponding
+; word at address offset by 0x60 to the instruction.
+; This setup allows to overlay addresses 0x40 to 0x5f
+; by Parameter Overlay Module (POM) when application
+; is started
 
 resetEntry
-        b   _c_int00
-undefEntry
-        b   undefEntry
-svcEntry
-        b   svcEntry
-prefetchEntry
-        b   prefetchEntry
-        b   _dabort
-reservedEntry
-        b   reservedEntry
-        ldr pc,[pc,#-0x1b0]
-        ldr pc,[pc,#-0x1b0]
+        b   _c_int00         ; 0x00
+        ldr pc,[pc,#0x58]    ; 0x04 undefined
+        ldr pc,[pc,#0x58]    ; 0x08 SVC
+        ldr pc,[pc,#0x58]    ; 0x0C prefetch abort
+        ldr pc,[pc,#0x58]    ; 0x10 data abort
+        ldr pc,[pc,#0x58]    ; 0x14 reserved
+        ldr pc,[pc,#0x58]    ; 0x18 IRQ
+        ldr pc,[pc,#0x58]    ; 0x1C FIQ
+; Above instructions are coded as 0xe59ff058
 
-    
+; Initial content and processing of exceptions
+; used by startup code
+
+        b   _c_int00         ; 0x20
+undefEntry
+        b   undefEntry       ; 0x24
+svcEntry
+        b   svcEntry         ; 0x28
+prefetchEntry
+        b   prefetchEntry    ; 0x2C
+b_to_dabort
+        b   _dabort          ; 0x30
+reservedEntry
+        b   reservedEntry    ; 0x34
+b_to_IRQVECREG
+        ldr pc,[pc,#-0x1d0]  ; 0x38 -> FFFFFE70h IRQVECREG
+b_to_FIQVECREG
+        ldr pc,[pc,#-0x1d0]  ; 0x3C -> FFFFFE74h FIQVECREG
+
+; Space from 0x40 to 0x5f is reserved to align target address
+; entries to location where is would be in 64 byte POM block
+; if standard sequence with
+;       ldr pc,[pc,#0x18]
+; is used
+
+        ldr pc,[pc,#0x18]
+        ldr pc,[pc,#0x18]
+        ldr pc,[pc,#0x18]
+        ldr pc,[pc,#0x18]
+        ldr pc,[pc,#0x18]
+        ldr pc,[pc,#0x18]
+        ldr pc,[pc,#0x18]
+        ldr pc,[pc,#0x18]
+
+; Area with target addresses of exception processing
+; routines
+
+        .long _c_int00       ; 0x60
+        .long undefEntry     ; 0x64
+        .long svcEntry       ; 0x68
+        .long prefetchEntry  ; 0x6C
+        .long b_to_dabort    ; 0x70
+        .long reservedEntry  ; 0x74
+        .long b_to_IRQVECREG ; 0x78
+        .long b_to_FIQVECREG ; 0x7C
+
 ;-------------------------------------------------------------------------------
